be5cd46c3ea89a10ba166d19ed6b48e1da3871d9
Pablo F. Mescher <pfmescher@gmail.com>
2023-01-20 17:49:49 -0300

    GET /jobs/unpaid

--- src/routes/app.js
+++ src/routes/app.js
@@ -7,12 +7,13 @@ const { Op } = require('sequelize');
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
+app.use(getProfile);
 
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id', getProfile, async (req, res) => {
+app.get('/contracts/:id', async (req, res) => {
   const { Contract } = req.app.get('models');
   const { id } = req.params;
   const contract = await Contract.findOne({
@@ -29,9 +30,49 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   res.json(contract);
 });
 
+/**
+ * Returns a list of contracts belonging to a user (client or contractor), the list should only contain non terminated contracts.
+ */
 app.get('/contracts', async (req, res) => {
   const { Contract } = req.app.get('models');
-  const contracts = await Contract.findAll({ where: { status: { [Op.ne]: 'terminated' } } });
+  const contracts = await Contract.findAll({
+    where: {
+      status: { [Op.ne]: 'terminated' },
+      [Op.or]: {
+        ClientId: req.profile.id,
+        ContractorId: req.profile.id
+      }
+    }
+  });
   res.json(contracts);
 });
+
+/**
+ * Get all unpaid jobs for a user (**_either_** a client or contractor), for **_active contracts only_**
+ */
+app.get('/jobs/unpaid', async (req, res) => {
+  const { Contract, Job } = req.app.get('models');
+  const contract = await Contract.findOne({
+    where: {
+      [Op.or]: {
+        ClientId: req.profile.id,
+        ContractorId: req.profile.id
+      },
+      status: 'in_progress'
+    },
+    include: [
+      {
+        model: Job,
+        where: {
+          paid: {
+            [Op.eq]: null
+          }
+        }
+      }
+    ]
+  });
+
+  res.json(contract?.Jobs || []);
+});
+
 module.exports = app;
